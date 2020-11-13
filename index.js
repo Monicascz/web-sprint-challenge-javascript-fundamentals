1eebdfdc60f451fe9531ed7c35e0c9c475890cad
Monica Zwissler <mzwissler@Monicas-MacBook-Air.local>
2020-11-13 10:29:40 -0800

    Create a CodeGrade submission

--- index.js
+++ index.js
@@ -23,12 +23,15 @@ myFunction();
 
 /* 🚀🚀🚀 Task 2: Counter 🚀🚀🚀 */
 /* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. Note, you may use a for loop for this function if you wish */
-// function summation() {
-//   /*Your Code Here*/
+function summation(number){
+  let counter = 0;
+  for (let i = 1; i <= number; i++){
+    counter = counter + i;
+  }
+  return counter;
+} console.log(summation(4));
 
-//   }
 
-function summation(){}
  
 
 // 🦁🦁🦁 Topic 2: ADVANCED Array Methods 🦁🦁🦁
